b669332acbb5dca206d35de96781a12d0dfb4872
williamweltlich <112646950+williamweltlich@users.noreply.github.com>
2022-09-05 20:13:17 -0500

    Update q2.cpp

--- assignment_1/q2.cpp
+++ assignment_1/q2.cpp
@@ -1,13 +1,15 @@
+//
+// Created by willi on 9/5/2022.
+// Question 2 Assignment 1
+//
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
-int main(){
-
-    int first = 5; 
-    double second = 7; 
-
-    cout << "first is: " << first << " " << "second is: " << second << endl ; 
-
-    return 0; 
+int main() {
+    int a;
+    cout <<"Enter a number\n";
+    cin>>a;
+    cout <<a <<" " <<a*2 <<" " <<a*a;
+    return 0;
 }
